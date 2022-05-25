c2fa6590cc56516857b0710a1c17269b6f38a4eb
ericstretton <eric.stretton@gmail.com>
2022-05-24 20:57:42 -0600

    Adding numbers to be called before declaring either fizz, buzz or fizzbuzz.

--- fizz.py
+++ fizz.py
@@ -2,11 +2,11 @@
 def fizzBuzz(i):
     for i in range(1, 51):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            print(str(i) + " FizzBuzz")
         elif i % 3 == 0:
-            print("Fizz")
+            print(str(i) + " Fizz")
         elif i % 5 == 0:
-            print("Buzz")
+            print(str(i) + " Buzz")
         else:
             print(i)
 i=1
